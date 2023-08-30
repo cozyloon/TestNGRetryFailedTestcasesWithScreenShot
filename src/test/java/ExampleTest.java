5440ea7fb386afc2e3d145e9dc348744276397b4
Chathumal Sangeeth <38062287+cozyloon@users.noreply.github.com>
2023-08-30 11:43:33 +0530

    Update ExampleTest.java

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,24 +1,51 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class ExampleTest {
 
-    SoftAssert softAssert =new SoftAssert();
-    @Test
-    public void test(){
-        WebDriver driver =new ChromeDriver();
+    SoftAssert softAssert = new SoftAssert();
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test() {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).submit();
         boolean app_logo = driver.findElement(By.className("app_logo")).isDisplayed();
-        driver.quit();
-        softAssert.assertEquals(app_logo,false);
+        softAssert.assertEquals(app_logo, false);
         softAssert.assertAll();
+    }
 
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            try {
+                TakesScreenshot screenshotDriver = (TakesScreenshot) driver;
+                File screenshotFile = screenshotDriver.getScreenshotAs(OutputType.FILE);
+                Files.move(screenshotFile.toPath(), new File("src/test/java/" + result.getName() + ".png").toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        driver.quit();
     }
 }
